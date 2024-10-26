d80d1c04e8593d12f68fdd9b1715f450062a4127
geethikasunkara1519 <155763075+geethikasunkara1519@users.noreply.github.com>
2024-10-26 11:55:56 +0530

    Add files via upload

---
+++ Geocart/script.js
@@ -0,0 +1,8 @@
+script.js
+// Mobile menu toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
